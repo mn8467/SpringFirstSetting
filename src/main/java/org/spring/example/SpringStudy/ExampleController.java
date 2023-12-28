df5f9ee45a50d3f8e02e2cf1e3f2f2bf8210a6f7
mn8467 <mn8467@naver.com>
2023-12-28 17:28:32 +0900

    jdbc연동, 필수태그추가

--- src/main/java/org/spring/example/SpringStudy/ExampleController.java
+++ src/main/java/org/spring/example/SpringStudy/ExampleController.java
@@ -1,4 +1,4 @@
-package org.spring.example.Controller;
+package org.spring.example.SpringStudy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
